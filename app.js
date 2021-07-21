1dd1196d8d72988c6b3b070bf981e66abee5173a
grace-kimm <38813861+grace-kimm@users.noreply.github.com>
2021-07-21 16:05:19 +0900

    complete todo app
    complete todo app according to the lecture

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("Your first JS project!")
+let input = prompt("What would you like to do?");
+const todos = ["collect eggs", "clean house"];
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("***************");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("***************");
+  } else if (input === "new") {
+    const newTodo = prompt("What is the new todo?");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to list!`);
+  } else if (input === "delete") {
+    const index = parseInt(prompt("What index do you want to delete?"));
+    if (!Number.isNaN(index)) {
+      const deleted = todos.splice(index, 1);
+      console.log(`Ok, deleted ${deleted[0]}`);
+    } else {
+      console.log("Unknown index");
+    }
+  }
+  input = prompt("What would you like to do?");
+}
+console.log("OK QUIT THE APP");
